f01b524fd898e0066cc3d01f0f3e2de73696056e
LG <gl717@outlook.com>
2019-10-18 14:05:06 +0800

    finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post signup_path, params: {user: {name: "", email: "user@123", password: "foo", password_confirmation: "bar"}}
+    end
+    assert_template 'users/new'
+    # assert_select 'div#<CSS id for error explanation'
+    # assert_select 'div.<CSS class for field with error>'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post signup_path, params: {user: {name: "user", email: "user@qq.com", password: "123456", password_confirmation: "123456"}}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
+end
